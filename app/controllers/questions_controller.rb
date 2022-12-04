db8a32940b393810b8d298fb9fc505e70e2cc4a8
drobinin575 <drobinin575@gmail.com>
2022-12-04 12:04:09 +0400

    initial commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,41 @@
+class QuestionsController < ApplicationController
+  def create
+    question = Question.create(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+      )
+    redirect_to question_path(question)
+  end
+
+  def update
+    question = Question.find(params[:id])
+
+    question.update(
+      body: params[:question][:body],
+      user_id: params[:question][:user_id]
+      )
+
+    redirect_to question_path(question)
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+end
