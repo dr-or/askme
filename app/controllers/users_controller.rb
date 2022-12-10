9bf5d66260a113d4b7ba1895eb46a097147fcd1d
drobinin575 <drobinin575@gmail.com>
2022-12-10 18:26:09 +0400

    added email validation, errors.full_messages, callback downcase_nickname, user`s editing and deletion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,51 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update destroy]
+
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
 
-    redirect_to root_path, notice: 'you signed up successfully'
+      redirect_to root_path, notice: 'you signed up successfully'
+    else
+      flash.now[:alert] = 'data invalid'
+
+      render :new
+    end
   end
 
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'your data is updated'
+    else
+      flash.now[:alert] = 'data invalid'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'your account was deleted'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
