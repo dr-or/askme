45825342b0e3b568ba8992c3eec45b67daa3707e
drobinin575 <drobinin575@gmail.com>
2023-01-30 18:48:47 +0400

    hashtag-question associations through hashtag_questions

--- app/models/hashtag_question.rb
+++ app/models/hashtag_question.rb
@@ -1,4 +1,4 @@
 class HashtagQuestion < ApplicationRecord
   belongs_to :hashtag
-  belongs_to :question
+  belongs_to :question, optional: true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,7 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
 
   belongs_to :author, class_name: 'User', optional: true
 
