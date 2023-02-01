05649778c06eb2457b13f5e03f8fb741fee0fbd4
drobinin575 <drobinin575@gmail.com>
2023-02-01 19:27:16 +0400

    search route & question helper for hashtag link

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def check_hashtags(text)
+    text.gsub(/(#[\w-]+)/) { search_questions($1) }
+  end
+
+  def search_questions(hashtag)
+    link_to hashtag, search_questions_path(hashtag: hashtag.delete("#"))
+  end
 end
